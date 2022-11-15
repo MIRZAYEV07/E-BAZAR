05cbfb71776d07f9dbb79a48ce2a3c52ca3f9d8e
mirzoyevogabek1 <mirzoyevogabek1@gmail.com>
2022-11-15 14:56:12 +0500

    order was modified

--- main/apps/order/managers.py
+++ main/apps/order/managers.py
@@ -4,37 +4,24 @@ from .utils import generate_random_string
 
 class OrderManager(BaseManager):
     def create_order_instance(self, user, data):
-        from ..account.models.user_book import BOOKSTATEChoices, UserBook
-        from ..book.models.book_type import TYPEChoices
+
+        from ..cart.models import TotalCartModel
         from .models import PAYMENTTypeChoices
 
         is_paid = False
         is_completed = False
-        price = data.get("book_type").price
+        price = data.get("cart").total_price
         payment_type = data.get("payment_type")
         quantity = data.get("quantity", 1)
 
-        # TODO: send PAYMe to subtract balance
-        if data.get("book_type").book_type == TYPEChoices.PAPER:
-            price *= quantity
+
+
         if payment_type == PAYMENTTypeChoices.ONLINE:
             is_paid = True
             is_completed = True
             user.create_balance(-price)
 
-        """
-        The given book should be added to user book list
-        """
-        if (data.get("book_type").book_type == TYPEChoices.ONLINE) or (
-            data.get("book_type").book_type == TYPEChoices.AUDIO
-        ):
-            UserBook.objects.get_or_create(
-                category=data.get("book").category,
-                owner=user,
-                book=data.get("book"),
-                state=BOOKSTATEChoices.PERMANENT,
-                book_type=data.get("book_type").book_type,
-            )
+
 
         return self.create(
             owner=user,
